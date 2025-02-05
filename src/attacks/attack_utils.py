d7a26ab9d8d8978377613afa89c7d76c003db11d
Will <wcorcoran@ucsb.edu>
2025-02-05 13:43:28 -0800

    added seeding

---
+++ src/attacks/attack_utils.py
@@ -0,0 +1,151 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import sys
+from sklearn.model_selection import train_test_split
+import torch.sparse as ts
+
+def likelihood_ratio_filter(node_pairs, modified_adjacency, original_adjacency, d_min, threshold=0.004):
+    """
+    Filter the input node pairs based on the likelihood ratio test proposed by Zügner et al. 2018, see
+    https://dl.acm.org/citation.cfm?id=3220078. In essence, for each node pair return 1 if adding/removing the edge
+    between the two nodes does not violate the unnoticeability constraint, and return 0 otherwise. Assumes unweighted
+    and undirected graphs.
+    """
+
+    N = int(modified_adjacency.shape[0])
+
+    original_degree_sequence = original_adjacency.sum(0)
+    current_degree_sequence = modified_adjacency.sum(0)
+
+    # Concatenate the degree sequences
+    concat_degree_sequence = torch.cat((current_degree_sequence, original_degree_sequence))
+
+    # Compute the log likelihood values of the original, modified, and combined degree sequences.
+    ll_orig, alpha_orig, n_orig, sum_log_degrees_original = degree_sequence_log_likelihood(original_degree_sequence, d_min)
+    ll_current, alpha_current, n_current, sum_log_degrees_current = degree_sequence_log_likelihood(
+        current_degree_sequence, d_min)
+
+    ll_comb, alpha_comb, n_comb, sum_log_degrees_combined = degree_sequence_log_likelihood(concat_degree_sequence, d_min)
+
+    # Compute the log likelihood ratio
+    current_ratio = -2 * ll_comb + 2 * (ll_orig + ll_current)
+
+    # Compute new log likelihood values that would arise if we add/remove the edges corresponding to each node pair.
+
+    new_lls, new_alphas, new_ns, new_sum_log_degrees = updated_log_likelihood_for_edge_changes(node_pairs,
+                                                                                               modified_adjacency, d_min)
+
+    # Combination of the original degree distribution with the distributions corresponding to each node pair.
+    n_combined = n_orig + new_ns
+    new_sum_log_degrees_combined = sum_log_degrees_original + new_sum_log_degrees
+    alpha_combined = compute_alpha(n_combined, new_sum_log_degrees_combined, d_min)
+
+    new_ll_combined = compute_log_likelihood(n_combined, alpha_combined, new_sum_log_degrees_combined, d_min)
+    new_ratios = -2 * new_ll_combined + 2 * (new_lls + ll_orig)
+
+    # Allowed edges are only those for which the resulting likelihood ratio measure is < than the threshold
+    allowed_edges = new_ratios < threshold
+    try:
+        filtered_edges = node_pairs[allowed_edges.cpu().numpy().astype(bool)]
+    except:
+        filtered_edges = node_pairs[allowed_edges.cpu().numpy().astype(bool)]
+
+    allowed_mask = torch.zeros(modified_adjacency.shape)
+    allowed_mask[filtered_edges.T] = 1
+
+    #The clone was added to ensure it didnt throw an error when adding
+    allowed_mask += allowed_mask.clone().t()
+    return allowed_mask, current_ratio
+
+
+def degree_sequence_log_likelihood(degree_sequence, d_min):
+    """
+    Compute the (maximum) log likelihood of the Powerlaw distribution fit on a degree distribution.
+
+    """
+    # Determine which degrees are to be considered, i.e. >= d_min.
+
+    D_G = degree_sequence[(degree_sequence >= d_min.item())]
+    try:
+        sum_log_degrees = torch.log(D_G).sum()
+    except:
+        sum_log_degrees = np.log(D_G).sum()
+    n = len(D_G)
+
+    alpha = compute_alpha(n, sum_log_degrees, d_min)
+    ll = compute_log_likelihood(n, alpha, sum_log_degrees, d_min)
+    return ll, alpha, n, sum_log_degrees
+
+def updated_log_likelihood_for_edge_changes(node_pairs, adjacency_matrix, d_min):
+
+    # For each node pair find out whether there is an edge or not in the input adjacency matrix.
+
+    edge_entries_before = adjacency_matrix[node_pairs.T]
+
+    degree_sequence = adjacency_matrix.sum(1)
+
+    D_G = degree_sequence[degree_sequence >= d_min.item()]
+    sum_log_degrees = torch.log(D_G).sum()
+    n = len(D_G)
+
+
+    deltas = -2 * edge_entries_before + 1
+    d_edges_before = degree_sequence[node_pairs]
+
+    d_edges_after = degree_sequence[node_pairs] + deltas[:, None]
+
+    # Sum the log of the degrees after the potential changes which are >= d_min
+    sum_log_degrees_after, new_n = update_sum_log_degrees(sum_log_degrees, n, d_edges_before, d_edges_after, d_min)
+
+    # Updated estimates of the Powerlaw exponents
+    new_alpha = compute_alpha(new_n, sum_log_degrees_after, d_min)
+    # Updated log likelihood values for the Powerlaw distributions
+    new_ll = compute_log_likelihood(new_n, new_alpha, sum_log_degrees_after, d_min)
+
+    return new_ll, new_alpha, new_n, sum_log_degrees_after
+
+
+def update_sum_log_degrees(sum_log_degrees_before, n_old, d_old, d_new, d_min):
+    # Find out whether the degrees before and after the change are above the threshold d_min.
+    old_in_range = d_old >= d_min
+    new_in_range = d_new >= d_min
+    d_old_in_range = d_old * old_in_range.float()
+    d_new_in_range = d_new * new_in_range.float()
+
+    # Update the sum by subtracting the old values and then adding the updated logs of the degrees.
+    sum_log_degrees_after = sum_log_degrees_before - (torch.log(torch.clamp(d_old_in_range, min=1))).sum(1) \
+                                 + (torch.log(torch.clamp(d_new_in_range, min=1))).sum(1)
+
+    # Update the number of degrees >= d_min
+    new_n = n_old - (old_in_range!=0).sum(1) + (new_in_range!=0).sum(1)
+    new_n = new_n.float()
+    return sum_log_degrees_after, new_n
+
+def compute_alpha(n, sum_log_degrees, d_min):
+
+    try:
+        alpha =  1 + n / (sum_log_degrees - n * torch.log(d_min - 0.5))
+    except:
+        alpha =  1 + n / (sum_log_degrees - n * np.log(d_min - 0.5))
+    return alpha
+
+def compute_log_likelihood(n, alpha, sum_log_degrees, d_min):
+    # Log likelihood under alpha
+    try:
+        ll = n * torch.log(alpha) + n * alpha * torch.log(d_min) + (alpha + 1) * sum_log_degrees
+    except:
+        ll = n * np.log(alpha) + n * alpha * np.log(d_min) + (alpha + 1) * sum_log_degrees
+
+    return ll
+
+def ravel_multiple_indices(ixs, shape, reverse=False):
+    """
+    "Flattens" multiple 2D input indices into indices on the flattened matrix, similar to np.ravel_multi_index.
+    Does the same as ravel_index but for multiple indices at once.
+    Parameters
+    """
+    if reverse:
+        return ixs[:, 1] * shape[1] + ixs[:, 0]
+
+    return ixs[:, 0] * shape[1] + ixs[:, 1]
